1ac930e31b4b789412d8f9d0a4ff9229bcbdfccd
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-07-31 19:17:26 +0100

    adds chakra theme

--- src/@chakra-ui/gatsby-plugin/theme.ts
+++ src/@chakra-ui/gatsby-plugin/theme.ts
@@ -1,22 +1,46 @@
 // src/@chakra-ui/gatsby-plugin/theme.js
-import { extendTheme, withDefaultColorScheme } from "@chakra-ui/react";
+import {
+  ComponentStyleConfig,
+  extendTheme,
+  withDefaultColorScheme,
+} from "@chakra-ui/react";
+
+const Button: ComponentStyleConfig = {
+  baseStyle: {
+    borderRadius: "3xl",
+    textTransform: "uppercase",
+    fontWeight: 500,
+    fontFamily: "heading",
+    _active: { transform: "scale(0.95)" },
+    transition: "all 0.25s cubic-bezier(0.645,0.045,0.355,1)",
+  },
+  defaultProps: {},
+};
 
 const theme = {
+  fonts: {
+    heading: `'Montserrat', sans-serif`,
+    body: `'Open Sans', sans-serif`,
+  },
   colors: {
-    primary: "#d49945",
+    primary: "#EF494B",
 
     main: {
-      50: "#fff3e0",
-      100: "#f3ddbd",
-      200: "#e8c696",
-      300: "#deb06e",
-      400: "#d49945",
-      500: "#bb802d",
-      600: "#916321",
-      700: "#684717",
-      800: "#3f2b09",
-      900: "#1a0d00",
+      50: "#ffe5e5",
+      100: "#fdb7b8",
+      200: "#f6898b",
+      300: "#f15b5d",
+      400: "#eb2e2f",
+      500: "#d21616",
+      600: "#a40e10",
+      700: "#76080b",
+      800: "#480305",
+      900: "#1e0000",
     },
+  },
+
+  components: {
+    Button,
   },
 };
 
