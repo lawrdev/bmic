93d312253e9fbb681b4884f25c7862e4d6232bdd
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-07-31 19:17:49 +0100

    adds container

---
+++ src/components/atoms/Container.tsx
@@ -0,0 +1,22 @@
+import { Box, BoxProps } from "@chakra-ui/react";
+import React from "react";
+
+export default function Container({
+  children,
+  boxProps,
+}: {
+  children: React.ReactNode;
+  boxProps?: BoxProps;
+}) {
+  return (
+    <Box
+      px={{ base: 6, md: "50px" }}
+      maxWidth={"1380px"}
+      width={"100%"}
+      mx={"auto"}
+      {...boxProps}
+    >
+      {children}
+    </Box>
+  );
+}
