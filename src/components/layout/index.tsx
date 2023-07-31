a185b06d3dc16aac4a52bb6b34195b4876e6b8aa
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-07-31 19:19:29 +0100

    feat: adds layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,8 +1,21 @@
+import "./layout.css";
+import "@fontsource/montserrat/300.css";
+import "@fontsource/montserrat/400.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/600.css";
+import "@fontsource/montserrat/700.css";
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/500.css";
+import "@fontsource/open-sans/600.css";
+import "@fontsource/open-sans/700.css";
+import "@fontsource/open-sans/800.css";
 import React from "react";
+
 import { ReactNode, FC } from "react";
 import { Box, Flex } from "@chakra-ui/react";
-import { Header } from "../organisms/Header";
 import Footer from "../organisms/Footer";
+import { Header } from "../organisms/Header";
 
 const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
