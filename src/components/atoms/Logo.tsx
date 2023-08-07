6d78439215fb10b9ceed7d356ad1e4e71ce16816
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-08-07 09:37:43 +0100

    style: dynamic logo

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -3,47 +3,45 @@ import { Box, BoxProps, DarkMode, Text } from "@chakra-ui/react";
 import { Link } from "gatsby";
 
 export function Logo({
-  color = "white",
+  color = "#fff",
   boxProps,
 }: {
   color?: string;
   boxProps?: BoxProps;
 }) {
   return (
-    <DarkMode>
-      <Link to={"/"}>
-        <Box
-          border={"2px solid #fff"}
-          borderRadius={"sm"}
-          py={1.5}
-          px={2}
-          cursor={"pointer"}
-          {...boxProps}
+    <Link to={"/"}>
+      <Box
+        border={`2px solid ${color}`}
+        borderRadius={"sm"}
+        py={1.5}
+        px={2}
+        cursor={"pointer"}
+        {...boxProps}
+      >
+        <Text
+          color={color}
+          fontFamily={"heading"}
+          fontSize={"10px"}
+          textTransform={"uppercase"}
+          fontWeight={700}
+          letterSpacing={"3px"}
+          lineHeight={"12px"}
         >
-          <Text
-            color={color}
-            fontFamily={"heading"}
-            fontSize={"10px"}
-            textTransform={"uppercase"}
-            fontWeight={700}
-            letterSpacing={"3px"}
-            lineHeight={"12px"}
-          >
-            BMI
-          </Text>
-          <Text
-            color={color}
-            fontFamily={"heading"}
-            fontSize={"10px"}
-            textTransform={"uppercase"}
-            fontWeight={700}
-            letterSpacing={"3px"}
-            lineHeight={"12px"}
-          >
-            CAL
-          </Text>
-        </Box>
-      </Link>
-    </DarkMode>
+          BMI
+        </Text>
+        <Text
+          color={color}
+          fontFamily={"heading"}
+          fontSize={"10px"}
+          textTransform={"uppercase"}
+          fontWeight={700}
+          letterSpacing={"3px"}
+          lineHeight={"12px"}
+        >
+          CAL
+        </Text>
+      </Box>
+    </Link>
   );
 }
